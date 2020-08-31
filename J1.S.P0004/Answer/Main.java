47b5b7b4351efdfcaae484ff43b56d1d57eea4f4
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2020-08-31 10:20:25 +0700

    upload J1.S.P0004

--- J1.S.P0004/Answer/Main.java
+++ J1.S.P0004/Answer/Main.java
@@ -1,51 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package j1.s.p0004;
+package Answer;
 
 import java.util.Random;
 import java.util.Scanner;
-/**
- *
- * @author admin
- */
-public class J1SP0004 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
         int[] arrayNumber = new int[inputNumberOfArray()];
 
         inputElements(arrayNumber);
         System.out.print("Unsorted array: ");
         displayArray(arrayNumber);
-        quicksort(arrayNumber, 0, arrayNumber.length);
+        quickSort(arrayNumber, 0, arrayNumber.length);
         System.out.print("Sorted array: ");
         displayArray(arrayNumber);
     }
-    
-    // function use quicksort sort to sort array
+
     public static void quicksort(int[] array, int begin, int end) {
         if (begin < end - 1) {        
             int point = pactition(array, begin, end);
         
-            quicksort(array, begin, point);
+            quicksort(array, begin, point - 1);
             quicksort(array, point + 1, end);
         }
     }
     
-    //function to sort
     public static int pactition (int[] array, int begin, int end) {
         int pilot = end - 1;
         int index_number_min = begin - 1;
         
-        //index run begin to end in the array
         for (int index = begin; index < end - 1; index++) {
-            //when array[index] <= array[pilot] --> count index min to 1 and swap index and index min
             if (array[index] <= array[pilot]) {
                 index_number_min ++;
                 int temp = array[index];
@@ -53,14 +36,7 @@ public class J1SP0004 {
                 array[index_number_min] = temp;
             }
         }
-        
-        //after that swap pilot to after index min
-        int temp = array[pilot];
-        array[pilot] = array[index_number_min + 1];
-        array[index_number_min + 1] = temp;
-        displayArray(array);
-        return index_number_min + 1;
-    }
+    
 
     // function to enter each elements with random
     public static void inputElements(int[] array) {
